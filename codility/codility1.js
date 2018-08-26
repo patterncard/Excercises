ad9c27ca37d0c5b07bd60814cf771c2eb7188187
patterncard <patcad@o2.pl>
2018-08-26 23:40:00 +0200

    range

---
+++ codility/codility1.js
@@ -0,0 +1,13 @@
+const _ = require('lodash');
+
+function driving(n) {
+  if (n >= 1 && n <= 100) {
+    return _.range(n).map(function(x, i) {
+      
+      return i}
+    )
+  }
+}
+console.log(driving(10));
+
+
